1c3d6f0f384f92bf7714745f47e92b5753ecf46a
hyeonik7 <hyeonik7@gmail.com>
2021-02-19 23:21:35 +0900

    Init simple spring boot example from https://start.spring.io/
    Signed-off-by: hyeonik7 <hyeonik7@gmail.com>

---
+++ src/main/kotlin/org/sb/practice/SpringBootPractice/SpringBootPracticeApplication.kt
@@ -0,0 +1,11 @@
+package org.sb.practice.SpringBootPractice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootPracticeApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringBootPracticeApplication>(*args)
+}
---
+++ src/test/kotlin/org/sb/practice/SpringBootPractice/SpringBootPracticeApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.sb.practice.SpringBootPractice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBootPracticeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
